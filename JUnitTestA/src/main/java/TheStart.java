596260e851d128cc1500a4700501983bd8a5c812
Jesse3evans <78041192+Jesse3evans@users.noreply.github.com>
2021-07-06 09:46:56 -0600

    The changes i made for the assignment

---
+++ JUnitTestA/src/main/java/TheStart.java
@@ -0,0 +1,23 @@
+
+public class TheStart {
+
+	public static void main(String[] args) {
+		
+		int intReturn;
+		
+		int a;
+		int b;
+		int c;
+		int d;
+		String alpha;
+		String bravo;
+		String charlie;
+		
+		
+		Calculator clsCalc = new Calculator();
+		intReturn = clsCalc.add(4,5);
+		System.out.println(intReturn);
+
+	}
+
+}
